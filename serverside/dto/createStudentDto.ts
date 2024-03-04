10fff8a9ae3a22f71ebb0e91806a2db3445a2448
Til Weber <78653138+tilwbr@users.noreply.github.com>
2024-03-04 15:24:33 +0100

    added ObjectDto for automatic validation

--- serverside/dto/createStudentDto.ts
+++ serverside/dto/createStudentDto.ts
@@ -1,7 +1,44 @@
-import { IsString, IsEmail, IsNotEmpty } from '@nestjs/class-validator';
+import {
+  IsString,
+  IsEmail,
+  IsNotEmpty,
+  IsObject,
+  IsNumber,
+  ValidateNested,
+} from '@nestjs/class-validator';
+import { userRole } from '@prisma/client';
+import { Type } from 'class-transformer';
+import { IsNotEmptyObject } from 'class-validator';
 
-export class CreateStudentDto {
+export abstract class User_Login_DataDto {
+  @IsNotEmpty()
+  @IsEmail()
+  readonly email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  readonly password: string;
+
+  // @IsNotEmpty()
+  // readonly role: userRole;
+}
+export abstract class CreateStudentDto {
+  @IsNotEmpty()
+  @IsObject()
+  @IsNotEmptyObject()
+  @ValidateNested()
+  @Type(() => User_Login_DataDto)
+  readonly user_Login_Data: User_Login_DataDto;
+
   @IsNotEmpty()
   @IsString()
   readonly name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  readonly lastname: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  readonly classID: number;
 }
