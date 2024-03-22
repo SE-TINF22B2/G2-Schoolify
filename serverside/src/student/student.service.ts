ac849f5fa189c3a9929fd8f6982637168a1e586c
Til Weber <78653138+tilwbr@users.noreply.github.com>
2024-03-22 11:01:59 +0100

    added functions

--- serverside/src/student/student.service.ts
+++ serverside/src/student/student.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { PrismaClient, Student } from '@prisma/client';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+  async getStudent(id: number, prisma: PrismaClient): Promise<Student> {
+    const student = await prisma.student.findUnique({
+      where: {
+        studentID: id,
+      },
+      select: {
+        studentID: true,
+        name: true,
+        lastname: true,
+        classClassID: true,
+        user_Login_DataUser_Login_DataID: true,
+        grades: {
+          select: {
+            gradeID: true,
+            grade: true,
+            testTestID: false,
+            Test: true,
+            teacherTeacherID: false,
+            Teacher: true,
+            subjectSubjectID: false,
+            Subject: true,
+          },
+        },
+      },
+    });
+    if (!student)
+      throw new HttpException(
+        'student with id ' + id + ' was not found',
+        HttpStatus.NOT_FOUND,
+      );
+    return student;
+  }
+}
