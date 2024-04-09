405c6ba0e4eb770c93dc37d039b6962835bcf001
Gerrit <gerrit.graetz@sap.com>
2024-04-09 18:22:30 +0200

    adden controller and service for getLessonForWeek

--- serverside/src/lesson/lesson.controller.ts
+++ serverside/src/lesson/lesson.controller.ts
@@ -1,6 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Headers } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { Get, Param } from '@nestjs/common';
 
 @ApiTags('Lesson')
 @Controller('lesson')
-export class LessonController {}
+export class LessonController {
+  @Get(':classId/week/:weekNumber')
+  getAllLessonsForClassInWeek(
+    @Headers('role') role,
+    @Param('classID') classId: number,
+    @Param('weekStart') weekNumber: number,
+  ) {
+    // Your code to fetch all lessons for a certain class in a week goes here
+  }
+}
--- serverside/src/lesson/lesson.service.ts
+++ serverside/src/lesson/lesson.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient, Lesson } from '@prisma/client';
+import moment from 'moment';
 
 @Injectable()
-export class LessonService {}
+export class LessonService {
+  async getLessonsForWeek(classID: number, weekStart: string, prisma: PrismaClient) : Promise<Lesson[]> {
+    const lessons: Lesson[] = await prisma.lesson.findMany({
+      where: {
+        classClassID: classID,
+        startTime : {
+          gte: new Date(weekStart),
+          lt: moment(weekStart).add(5, 'days').toDate(),
+        }
+      }
+    });
+    return lessons
+  }
+
+}
