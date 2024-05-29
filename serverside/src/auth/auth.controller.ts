a1f2ba534a50aec45f8ee4382cfcde9a2818bad6
Marvin <marvin.lindner@sap.com>
2024-05-29 17:27:15 +0200

    alte implementierung gelöscht

--- serverside/src/auth/auth.controller.ts
+++ serverside/src/auth/auth.controller.ts
@@ -1,8 +1,7 @@
 import { ApiTags } from '@nestjs/swagger';
-import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { Controller, Inject } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Prisma, PrismaClient } from '@prisma/client';
-import { User_Login_DataDto } from '../../dto/loginDTO';
 
 @ApiTags('Auth')
 @Controller('auth')
@@ -11,8 +10,4 @@ export class AuthController {
     private readonly authService: AuthService,
     @Inject('PRISMA') private prisma: PrismaClient<Prisma.PrismaClientOptions>,
   ) {}
-  @Post('logIn')
-  async logIn(@Body() loginData: User_Login_DataDto): Promise<boolean> {
-    return await this.authService.logIn(loginData, this.prisma);
-  }
 }
--- serverside/src/auth/auth.service.ts
+++ serverside/src/auth/auth.service.ts
@@ -1,28 +1,4 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
-import { User_Login_DataDto } from 'dto/loginDTO';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {
-  async logIn(
-    newLogin: User_Login_DataDto,
-    prisma: PrismaClient,
-  ): Promise<boolean> {
-    const student = await prisma.user_Login_Data.findUnique({
-      where: {
-        email: newLogin.email,
-      },
-    });
-    if (!student || student.password !== newLogin.password) {
-      throwNotFound();
-    }
-    return true;
-  }
-}
-
-function throwNotFound() {
-  throw new HttpException(
-    'Either the User does not exist or the password is wrong. Please try again!',
-    HttpStatus.BAD_REQUEST,
-  );
-}
+export class AuthService {}
