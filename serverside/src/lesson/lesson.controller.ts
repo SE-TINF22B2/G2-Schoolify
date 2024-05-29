f759f5a6fe5dcf4f6ca72fff025db3e6a3d43823
CodeRider101 <98173906+CodeRider101@users.noreply.github.com>
2024-05-29 22:30:11 +0200

    115 stundenplan backend (#317)
    * Logik und Tests implementiert
    
    * outsourced methods
    
    ---------
    
    Co-authored-by: Jeremie Bents <jeremie.bents@sap.com>

--- serverside/src/lesson/lesson.controller.ts
+++ serverside/src/lesson/lesson.controller.ts
@@ -1,6 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Inject, ParseIntPipe } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { Get, Param } from '@nestjs/common';
+import { LessonService } from './lesson.service';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 @ApiTags('Lesson')
 @Controller('lesson')
-export class LessonController {}
+export class LessonController {
+  constructor(
+    private readonly lessonService: LessonService,
+    @Inject('PRISMA') private prisma: PrismaClient<Prisma.PrismaClientOptions>,
+  ) {}
+
+  @Get('getLessonsForWeek/:weekStart/:classId')
+  async getLessonsForWeek(
+    //@Headers('role') role,
+    @Param('weekStart') weekStart: Date,
+    @Param('classID', new ParseIntPipe()) classID: number,
+  ) {
+    //keine Rolle abfragen, da jeder den Stundenplan betrachten kann
+    return await this.lessonService.getLessonsForWeek(
+      weekStart,
+      classID,
+      this.prisma,
+    );
+  }
+}
--- serverside/src/lesson/lesson.module.ts
+++ serverside/src/lesson/lesson.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
 import { LessonController } from './lesson.controller';
+import { LessonService } from './lesson.service';
+import { PrismaClient } from '@prisma/client';
 
 @Module({
+  providers: [
+    LessonService,
+    {
+      provide: 'PRISMA',
+      useValue: new PrismaClient(),
+    },
+  ],
   controllers: [LessonController],
 })
 export class LessonModule {}
