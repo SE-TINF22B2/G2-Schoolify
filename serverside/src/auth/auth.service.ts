9cae1309af7913a2dcf8438988cae0ff153f7175
Marvin <marvin.lindner@sap.com>
2024-06-10 18:50:17 +0200

    fix

--- serverside/src/auth/auth.service.ts
+++ serverside/src/auth/auth.service.ts
@@ -13,25 +13,26 @@ export class AuthService {
     prisma: PrismaClient,
   ): Promise<{ access_token: string }> {
     // user mit anhand email finden
-    const findUser = prisma.user_Login_Data.findUnique({
+    const findUser = await prisma.user_Login_Data.findUnique({
       where: {
         email: email,
       },
     });
 
+    console.log(findUser);
     // hash mash
     const saltOrRounds = 10;
     const hash = await bcrypt.hash(password, saltOrRounds);
 
     // user existiert nicht oder passwort falsch
-    if (!findUser || !(await bcrypt.compare((await findUser).password, hash))) {
+    if (!findUser || !(await bcrypt.compare(findUser.password, hash))) {
       throw new UnauthorizedException();
     }
 
     //payload für jwt
     const payload = {
-      sub: (await findUser).user_Login_DataID,
-      email: (await findUser).email,
+      sub: findUser.user_Login_DataID,
+      email: findUser.email,
     };
     return {
       access_token: await this.jwtService.signAsync(payload),
