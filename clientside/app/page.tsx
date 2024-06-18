974e7f741f77397deb76603e5b1aab5768803d78
Gerrit <gerrit.graetz@gmail.com>
2024-06-18 21:29:16 +0200

    added mock image for homepage

--- clientside/app/page.tsx
+++ clientside/app/page.tsx
@@ -1,7 +1,18 @@
+import { Image } from "@nextui-org/react";
 
 
 export default function Home() {
   return (
-<div></div>
+<div>
+<Image
+                shadow="sm"
+                radius="lg"
+                width="100%"
+                height="100%"
+                alt={"Startseite"}
+                className="w-full pt-5"
+                src={"/images/startseite.png"}
+              />
+</div>
   )
 }
